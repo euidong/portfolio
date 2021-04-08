e5c14b566a0639676112492872098760eb5cf6e7
justicedong <justicedong@naver.com>
2021-04-08 11:41:24 +0900

    Updates

---
+++ precache-manifest.6027536ef66bea38ff3cc0d875e68e18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47da44ba8bd11f69206067ada61f9e57",
+    "url": "/portpolio/index.html"
+  },
+  {
+    "revision": "8f4674f6d9d0f3faf7f9",
+    "url": "/portpolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "42049c4d1f168127a68b",
+    "url": "/portpolio/static/css/main.537fef90.chunk.css"
+  },
+  {
+    "revision": "8f4674f6d9d0f3faf7f9",
+    "url": "/portpolio/static/js/2.4a59a0b6.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portpolio/static/js/2.4a59a0b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42049c4d1f168127a68b",
+    "url": "/portpolio/static/js/main.5e25d610.chunk.js"
+  },
+  {
+    "revision": "d124d0ed90a8296f6e7c",
+    "url": "/portpolio/static/js/runtime-main.4f022238.js"
+  }
+]);
